178350f599bb2d3043051c18a3eb2565f0ff8f0f
Rob Sizeland <robcakes69@gmail.com>
2023-01-10 11:30:26 +0000

    Testing ES6

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,39 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     //Code Block
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(2, 6)
+// console.log(sum)
 
 // Arrow Function With Parameters
+// const addTwoNumbers = (a, b) => {
+//     //Code Block
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(2, 6)
+// console.log(sum)
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers = (a, b) => a + b;
+let sum = addTwoNumbers(5, 2);
+console.log(sum)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello!')
+
+const sayHello = () => console.log('Hello!')
+sayHello()
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string!
+    <p>`
+)
+console.log(returnMultipleLines())
